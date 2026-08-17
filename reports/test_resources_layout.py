1c27fa98aa46a27a0bf0c012c62a808fd29ede52
Papa Djibril DIAGNE <papa.diagne@neemba.com>
2026-08-17 16:20:37 +0000

    fix: fit resources workspace to viewport

---
+++ reports/test_resources_layout.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class ResourcesLayoutTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("resources-user", password="test", is_staff=True)
+        self.client.force_login(self.user)
+
+    def test_resources_workspace_uses_viewport_fitted_layout(self):
+        response = self.client.get(reverse("resources"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'class="resources-page-body"')
+        self.assertContains(response, "resource-library-shell")
+        self.assertContains(response, 'aria-label="Resource documents"')
+        self.assertNotContains(response, 'class="workspace-band"')
+
